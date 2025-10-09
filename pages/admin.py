394c232186cd8611137068aae6e7ced5236273f6
benoy_Judy <benoyjudy@gmail.com>
2025-10-09 13:25:03 +0530

    Team admin customisation done

--- pages/admin.py
+++ pages/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
 from .models import *
-
+from django.utils.html import format_html
 # Register your models here.
-admin.site.register(Team)
+
+
+class Teamadmin(admin.ModelAdmin):
+    
+    def thumbnail(self,object):
+        return format_html('<img src="{}"width="40" style="border-radius:30px">'.format(object.photo.url))
+    
+
+    thumbnail.short_description='Photo'
+        
+    list_display=('id','thumbnail','first_name','designation','created_date')
+    list_display_links=('id','first_name')
+
+    search_fields=('first_name','last_name','designation','id')
+
+    list_filter=('designation',)
+
+
+admin.site.register(Team,Teamadmin)
--- pages/models.py
+++ pages/models.py
@@ -6,12 +6,15 @@ class Team(models.Model):
     first_name=models.CharField(max_length=255)
     last_name=models.CharField(max_length=255)
     designation=models.CharField(max_length=255)
-    photo=models.ImageField(upload_to='photos/%Y/%M/%d/')
+    photo=models.ImageField(upload_to='%Y/%m/%d/')
     facebook_link=models.URLField(max_length=100)
-    twitter_link= models.URLField(max_length=100)
-    google_plus_link=models.URLField(max_length=100)
-    created_date=models.DateTimeField(auto_now_add=True)
+    twitter_link=models.URLField(max_length=100)
+    google_plus_link= models.URLField(max_length=100)
+    created_date= models.DateTimeField(auto_now_add=True)
 
+
+    # class Meta:
+    #     verbose_name_plural='Teams'
 
     def __str__(self):
         return self.first_name
